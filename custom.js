cd60bece946318ad48d41919435794b4beec251a
sharifpran84 <139911848+sharifpran84@users.noreply.github.com>
2023-09-03 23:19:58 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,11 @@
+$(".owl-carousel").owlCarousel({
+    items: 5,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin:30,
+    smartSpeed:1200,
+    active:true,
+    
+});
+
