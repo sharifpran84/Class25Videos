101a11021747b427857f5d27f18ccb4717cb1ea3
sharifpran84 <139911848+sharifpran84@users.noreply.github.com>
2023-09-04 14:17:52 +0600

    6th

--- custom.js
+++ custom.js
@@ -3,33 +3,31 @@ $(" #first .owl-carousel").owlCarousel({
     loop: true,
     autoplay: true,
     autoplayTimeout: 3000,
-    margin:30,
-    smartSpeed:1200,
-    active:true,
-    
+    margin: 30,
+    smartSpeed: 1200,
+    active: true,
 });
 $(" #secound .owl-carousel").owlCarousel({
     loop: true,
     autoplay: true,
-    margin:30,
+    margin: 30,
     autoplayTimeout: 5000,
-    smartSpeed:1200,
-    responsiveClass:true,
-    active:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+    smartSpeed: 1200,
+    responsiveClass: true,
+    active: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true,
         },
-        600:{
-            items:1,
-            nav:false
+        600: {
+            items: 1,
+            nav: false,
         },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false
-        }
-    }
+        1000: {
+            items: 1,
+            nav: true,
+            loop: false,
+        },
+    },
 });
-
